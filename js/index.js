2fd9297f980cc1f4cdc16d2e17a4c9272ca3c190
emmie-j <attachiegrace408@gmail.com>
2024-06-13 16:33:13 +0000

    updated

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('view')
+btn.onclick = function () {
+    alert('Where Imagination Meets Creativity')
+}
+
+const btnShop = document.getElementById('btn-btn')
+btnShop.onclick = function () {
+    alert("It's Sales Time")
+}

9befdb402b1311966e554ef74c4a2e56da220f47
emmie-j <attachiegrace408@gmail.com>
2024-06-14 12:17:54 +0000

    updated

--- js/index.js
+++ js/index.js
@@ -7,3 +7,19 @@ const btnShop = document.getElementById('btn-btn')
 btnShop.onclick = function () {
     alert("It's Sales Time")
 }
+
+const shop = document.getElementById('shop-now')
+shop.onclick = function () {
+    alert("Limited-time blowout sale")
+}
+
+// const log = document.getElementById('Log')
+// Log.onclick = function () {
+//     alert("")
+// }
+
+const Log = document.getElementById('Log');
+    Log.onclick = function() {
+        const name = prompt('Enter your full name.');
+        document.getElementById('name').innerText = name;
+    }
